bbfe947c1a360845ce81097db4589b141dee3a3c
Arpan007 <arpanm2092@gmail.com>
2026-04-12 23:34:10 +0530

    Move Firebase config to env

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,21 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// (optional) import { getAnalytics } from "firebase/analytics";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBxkuT-PEOSMMDj7rykNdm-hb54owvnEJA",
-    authDomain: "placemate-db.firebaseapp.com",
-    projectId: "placemate-db",
-    storageBucket: "placemate-db.firebasestorage.app",
-    messagingSenderId: "56706616861",
-    appId: "1:56706616861:web:5faa055ef49d8aa6ea80f4",
-    measurementId: "G-28E8DKT9E7"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+//(not needed as of now)
+// const analytics = getAnalytics(app);
+
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
